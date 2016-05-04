48b471d70029cda6de90862770a642589afcf0ff
Christian Kujau <lists@nerdbynature.de>
2016-05-04 15:34:11 -0700

    check_entropy.sh: monitors the available entropy of a Linux kernel.
    Signed-off-by: Christian Kujau <lists@nerdbynature.de>

---
+++ check_entropy.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+#
+# (c)2014 Christian Kujau <lists@nerdbynature.de>
+# Nagios plugin to check kernel.random.entropy_avail
+#
+# Similar plugins exist, but all seemed overly complicated and a bourne shell
+# version is nice because it doesn't need Perl or Python installed.
+#   Perl: http://www.unixadm.org/software/nagios-stuff/checks/check_entropy
+# Python: https://anonscm.debian.org/cgit/pkg-nagios/pkg-nagios-plugins-contrib.git/tree/dsa/checks/dsa-check-entropy
+#
+while getopts ":w:c:" opt; do
+	case $opt in
+		w)
+		warn=$OPTARG ;;
+
+		c)
+		crit=$OPTARG ;;
+	esac
+done
+
+# Both warn and crit are needed
+if [ -z "$warn" ] || [ -z "$crit" ]; then
+	echo "Usage: $(basename $0) -w num -c num"
+	exit 3
+fi
+
+# warn should be greater than crit	
+if [ $warn -lt $crit ]; then
+	echo "UNKNOWN: warn ($warn) < crit ($crit)"
+	exit 3
+fi
+
+# TODO: how about other operating systems?
+ENTROPY=$(/sbin/sysctl -n kernel.random.entropy_avail)
+if [ $ENTROPY -lt $crit ]; then
+	echo "CRITICAL: Too little entropy ($ENTROPY) in the pool (warn: $warn, crit: $crit)"
+	exit 2
+elif [ $ENTROPY -lt $warn ]; then
+	echo "WARNING: Too little entropy ($ENTROPY) in the pool (warn: $warn, crit: $crit)"
+	exit 2
+else
+	echo "OK: $ENTROPY bytes in the pool."
+	exit 0
+fi
